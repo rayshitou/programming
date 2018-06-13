60cddf9f2856073f35e3cf91cc7ea05096f37674
mac <mac@zldeMacBook-Pro.local>
2018-06-13 09:01:16 +0800

    RPC API for creating keys with map format returned

--- include/eosio/nxsd_api_plugin/nxsd_api_plugin.hpp
+++ include/eosio/nxsd_api_plugin/nxsd_api_plugin.hpp
@@ -5,6 +5,7 @@
 #pragma once
 #include <appbase/application.hpp>
 #include <eosio/http_plugin/http_plugin.hpp>
+#include <eosio/chain_plugin/chain_plugin.hpp>
 
 namespace eosio {
 
@@ -13,9 +14,13 @@ using namespace appbase;
 class nxsd_api_plugin : public appbase::plugin<nxsd_api_plugin> {
 public:
    nxsd_api_plugin();
+   nxsd_api_plugin(const nxsd_api_plugin&) = delete;
+   nxsd_api_plugin(nxsd_api_plugin&&) = delete;
+   nxsd_api_plugin& operator=(const nxsd_api_plugin&) = delete;
+   nxsd_api_plugin& operator=(nxsd_api_plugin&&) = delete;
    ~nxsd_api_plugin();
 
-   APPBASE_PLUGIN_REQUIRES((http_plugin))
+   APPBASE_PLUGIN_REQUIRES((http_plugin) (chain_plugin))
    virtual void set_program_options(options_description&, options_description& cfg) override;
  
    void plugin_initialize(const variables_map& options);
--- nxsd_api_plugin.cpp
+++ nxsd_api_plugin.cpp
@@ -16,3 +16,73 @@
 #include <boost/algorithm/clamp.hpp>
 
 #include <utility>
+
+namespace eosio { namespace detail {
+  struct nxsd_api_empty {};
+
+  struct nxsd_api_keys {
+     std::string pub_key;
+     std::string prv_key;
+  };
+}}
+
+FC_REFLECT(eosio::detail::nxsd_api_empty, );
+FC_REFLECT(eosio::detail::nxsd_api_keys, (pub_key)(prv_key));
+
+namespace eosio {
+
+static appbase::abstract_plugin& _nxsd_api_plugin = app().register_plugin<nxsd_api_plugin>();
+
+using namespace eosio::chain;
+using key_pair = std::pair<std::string, std::string>;
+using results_pair = std::pair<uint32_t,fc::variant>;
+
+#define CALL(api_name, api_handle, call_name, invoke_cb) \
+{std::string("/v1/" #api_name "/" #call_name), \
+   [this](string, string body, url_response_callback response_cb) mutable { \
+          try { \
+             if (body.empty()) body = "{}"; \
+             const auto result = api_handle->invoke_cb(body); \
+             response_cb(result.first, fc::json::to_string(result.second)); \
+          } catch (...) { \
+             http_plugin::handle_exception(#api_name, #call_name, body, response_cb); \
+          } \
+       }}
+
+struct nxsd_api_plugin_impl{
+    enum http_return_codes {
+      key_created = 201,
+      key_generated_failed = 419
+   };
+
+   results_pair create_key(const std::string& body){
+      auto pk    = private_key_type::generate();
+      auto privs = string(pk);
+      auto pubs  = string(pk.get_public_key());
+      eosio::detail::nxsd_api_keys keys{pubs, privs};
+      
+      return { key_created, fc::variant(keys) };
+   }
+};
+
+void nxsd_api_plugin::set_program_options(options_description&, options_description& cfg) {}
+
+void nxsd_api_plugin::plugin_initialize(const variables_map&) {}
+
+void nxsd_api_plugin::plugin_startup(){
+    ilog( "starting nxsd_api_plugin" );
+    app().get_plugin<http_plugin>().add_api({
+      CALL( nxsd, my, create_key, nxsd_api_plugin_impl::create_key )
+   });
+}
+
+void nxsd_api_plugin::plugin_shutdown(){
+
+}
+
+nxsd_api_plugin::nxsd_api_plugin()
+: my(new nxsd_api_plugin_impl())
+{}
+
+nxsd_api_plugin::~nxsd_api_plugin(){}
+}
