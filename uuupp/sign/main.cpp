2f38c9fcb9086a0e351bcab3fb10a67942ef85a9
陈春云 <app@appdeMacBook-Pro.local>
2018-08-28 18:46:43 +0800

    backup

--- uuupp/sign/main.cpp
+++ uuupp/sign/main.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include "sign.hpp"
+#include "exception.hpp"
 
 int main(int argc, char **argv){
         char sig[100] = {'\0'};
 	char *data = "you and me";
-        char *privkey = "5Jtfxa51FnM6QPQejXkwybYcwxDqs5EB1zk444K7eUPtDgk49rp";
-	sign(data, strlen(data), privkey, sig);
+        char *privkey = "5KYZdUEo39z3FPrtuX2QbbwGnNP5zTd7yyr2SC1j299sBCnWjss";
+        try {
+	 sign(data, strlen(data), privkey, sig);
+        } catch( const fc::exception& e ) {
+          printf("err msg: %s\n", e.to_detail_string().c_str());
+          return 1;
+	}
         printf("signature: %s\n", sig);
 	return 0;
 }
--- uuupp/sign/sign.cpp
+++ uuupp/sign/sign.cpp
@@ -2,8 +2,9 @@
 #include "sha256.hpp"
 
 #include <string.h>
-#include <stdio.h>
 #include "sign.hpp"
+
+#include "exception.hpp"
 
 using digest_type = fc::sha256;
 using private_key_type = fc::crypto::private_key;
@@ -13,18 +14,24 @@ int do_sign(const digest_type& digest, const char *prikey, char *sign_out)
 {
     std::string str_prikey = prikey;
     private_key_type pkey(str_prikey);
-    printf("key: %s\n", str_prikey.c_str());
-    signature_type sig = pkey.sign(digest);
-    std::string str_sig = std::string(sig);
-    printf("sig: %s\n", str_sig.c_str());
-    strncpy(sign_out, str_sig.c_str(), str_sig.size());
+    std::string str_sig("");
+    int itry_times = 1;
+    while(itry_times <= 1000){
+      try{
+    	signature_type sig = pkey.sign(digest);
+    	str_sig = std::string(sig);
+    	strncpy(sign_out, str_sig.c_str(), str_sig.size());
+        break;
+      } catch( const fc::exception& e ) {
+          ++itry_times;
+      }
+    }
     return str_sig.size();
 }
 int sign(const char *data, unsigned int length, const char *prikey, char *sign_out)
 {
     if( data && prikey && length > 0 && sign_out ) {
 	const digest_type& digest = digest_type::hash(data, length);
-        printf("ss: %s\n", std::string(digest).c_str());
         return do_sign(digest, prikey, sign_out);
     }
     return 0;
